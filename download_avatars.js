67df46d16f6b1d53289bbd8f6cfdd9dbd1bdd48b
quhairfoir <moragstc@gmail.com>
2018-08-06 19:28:09 -0400

    final version, tidied and comments added

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,9 @@
+// required modules
 var request = require('request');
 var secrets = require('./secrets');
 var fs = require('fs');
+
+// variables from modules and command line arguments
 var ghtoken = secrets.GITHUB_TOKEN;
 var args = process.argv.slice(2);
 var repoOwner = args[0];
@@ -8,6 +11,7 @@ var repoName = args[1];
 
 console.log("Welcome to the GitHub Avatar Downloader - v.Morag")
 
+// cb is the callback function, defined below
 function getRepoContributors(repoOwner, repoName, cb) {
 
   var options = {
@@ -18,9 +22,10 @@ function getRepoContributors(repoOwner, repoName, cb) {
     }
   };
 
+  // stop function if command line arguments not given
   if (repoOwner && repoName) {
 
-  request(options, function(err, res, body){
+  request(options, function(err, res, body) {
     var result = JSON.parse(body);
     cb(err, result);
     });
@@ -29,17 +34,21 @@ function getRepoContributors(repoOwner, repoName, cb) {
   }
 }
 
-getRepoContributors(repoOwner, repoName, cb);
-
-
-
+// function that loops over array of objects returned in body
 function cb(err, result) {
   result.forEach (function(result) {
     downloadImageByURL(result.avatar_url, ("avatars/" + result.login + ".jpg"));
   })
 };
 
-function downloadImageByURL(url, filePath){
+// downloads images and saves to new file in specified location
+function downloadImageByURL(url, filePath) {
   request.get(url)
   .pipe(fs.createWriteStream("./" + filePath));
 }
+
+//function call
+getRepoContributors(repoOwner, repoName, cb);
+
+
+
